9ac03ad82a4b1b6c692dfb63b5a982d9012cd2c7
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-07-30 22:28:00 +0530

    added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link, Links } from 'react-router-dom'
+import{  } from '../assets/assets'
 
 const navbar = () => {
   return (
     <div>
-      
+        
+      <img src="{assets.logo}" alt="logo" className="h-8"/>
     </div>
   )
 }
