9da7d93247e8071eb37f7d11c3634139b4335286
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-08-01 11:01:52 +0530

    path created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from 'react'
 import Navbar from './components/navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Car from './pages/Car'
+import CarDetails from './pages/CarDetails'
+import MyBookings from './pages/MyBookings'
 
 const App = () => {
-  const[showLogin, setShowLogin] = useState(false)
-  const isOwnerPath = useLocation().pathname.startWith ('/owner')
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startWith('/owner')
 
   return (
-    <div>
+    <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/car-details/:id' element={<CarDetails />} />
+        <Route path="/cars" element={<Car />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+      </Routes>
+    </>
   )
 }
 
