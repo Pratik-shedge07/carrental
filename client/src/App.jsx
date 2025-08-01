210bccca1426a86d7c2e6d7a487ffd6aeac9fa50
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-08-01 10:54:36 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/navbar'
+import { useLocation } from 'react-router-dom'
 
-function App() {
+const App = () => {
+  const[showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startWith ('/owner')
+
   return (
     <div>
-      <Navbar />
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
     </div>
   )
 }
